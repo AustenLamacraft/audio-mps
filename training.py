3a844c93f07b2f5dcd4d24c01b22abcc6c5125a9
mencia <beinat.mencia@gmail.com>
2018-10-10 12:00:34 +0100

    Added coherent state initial state to training

--- training.py
+++ training.py
@@ -3,11 +3,18 @@ import numpy as np
 
 from model import AudioMPS
 
-# PARAMETERS	
+# PARAMETERS
 
-BOND_D = 20
+BOND_D = 11 #ODDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 dt  = 0.001
-BATCH_SIZE = 20
+BATCH_SIZE = 8
+
+# COHERENT STATE
+
+theta = 2.*np.pi
+#phi = 2.*np.pi/3
+#phi = 4.*np.pi/3
+phi = 2.*np.pi
 
 # CHOOSE DATA
 
@@ -26,7 +33,8 @@ path = '_damped_sine_multirandomphase'
 # CHOOSE INITIAL STATE OF THE ANCILLA
 
 # path_is = '_pure'
-path_is = '_maximally_mixed'
+#path_is = '_maximally_mixed'
+path_is = '_coherent_pure'
 
 
 # CREATE DATA
@@ -92,7 +100,7 @@ elif path == '_damped_sine_2note':
 
 elif path == '_damped_sine_multirandomphase':
 
-        qq = np.reshape(np.random.uniform(np.zeros(BATCH_SIZE),2*np.pi*np.ones(BATCH_SIZE)),(BATCH_SIZE,1)) 
+        qq = np.reshape(np.random.uniform(np.zeros(BATCH_SIZE),2*np.pi*np.ones(BATCH_SIZE)),(BATCH_SIZE,1))
         INPUT_LENGTH = 50
         with tf.variable_scope("model_data", reuse=tf.AUTO_REUSE):
                 range_stack = tf.stack(BATCH_SIZE * [tf.range(1,INPUT_LENGTH,dtype=np.float32)])
@@ -114,7 +122,20 @@ if path_is == '_maximally_mixed':
 
 elif path_is == '_pure':
 
-	rho_0 = tf.constant([[1,0],[0,0]],dtype=tf.complex64)	 
+	rho_0 = tf.constant([[1,0],[0,0]],dtype=tf.complex64)
+
+elif path_is == '_coherent_pure':
+
+    j = (BOND_D - 1) / 2
+
+    mu = tf.range(-j, j + 1, dtype=tf.float32)
+    #####
+    a1 = tf.sqrt(tf.exp(tf.lgamma((2. * j) + 1)) / (tf.exp(tf.lgamma(j + mu + 1)) * tf.exp(tf.lgamma(j - mu + 1))))
+    a2 = (-tf.sin(theta / 2)) ** (j + mu)
+    a3 = (tf.cos(theta / 2)) ** (j - mu)
+    a4 = tf.exp(-1j * tf.cast((j + mu) * phi, tf.complex64))
+    cs = tf.cast(a1 * a2 * a3, tf.complex64) * a4
+    rho_0 = tf.einsum('i,j->ij', tf.conj(cs), cs)
 
 # CREATE THE OBJECT sine_model
 
@@ -127,11 +148,13 @@ tf.summary.scalar("loss_function", tf.reshape(our_model.loss, []))
 tf.summary.scalar("H_00", tf.reshape(our_model.H[0][0], []))
 tf.summary.scalar("R_00", tf.reshape(our_model.R[0][0], []))
 
-# global_step: Optional Variable to increment by one after the variables have been updated. 
+# global_step: Optional Variable to increment by one after the variables have been updated.
 
 step = tf.get_variable("global_step", [], tf.int64, tf.zeros_initializer(), trainable=False)
 train_op = tf.train.AdamOptimizer(1e-3).minimize(our_model.loss, global_step=step)
 
 # RUN THE TRAINING LOOP
 
-tf.contrib.training.train(train_op, logdir="../logging/logging_D"+str(BOND_D)+"_dt"+str(dt)+"_batchsize"+str(BATCH_SIZE)+path+path_is+"_neglect_first_term", save_checkpoint_secs=60)
+tf.contrib.training.train(train_op, logdir="../logging/logging_D"+str(BOND_D)+"_dt"+str(dt)+"_batchsize"+
+                                           str(BATCH_SIZE)+path+"_theta"+str(theta)+"_phi"+
+                                           str(phi)+path_is,save_checkpoint_secs=60)
