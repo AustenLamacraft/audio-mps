baf02ccc0a588db0e3d083c46edcc51b52b2a04e
Austen Lamacraft <austen.lamacraft@gmail.com>
2019-05-16 15:00:38 +0100

    More simple model tests.

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from model import CMPS, RhoCMPS, PsiCMPS
+from data import get_audio
 
 from tensorflow.contrib.training import HParams
 hparams = HParams(minibatch_size=8, bond_dim=8, delta_t=0.001,
@@ -20,7 +21,7 @@ class TestCMPS(tf.test.TestCase):
 
 class TestRhoCMPS(tf.test.TestCase):
 
-    def testRhoIsADensityMatrix(self):
+    def testRho0IsADensityMatrix(self):
 
         model = RhoCMPS(hparams)
 
@@ -28,6 +29,32 @@ class TestRhoCMPS(tf.test.TestCase):
             sess.run(tf.global_variables_initializer())
             self.assertAllClose(model.rho_0, model.rho_0 / tf.trace(model.rho_0))
             self.assertAllClose(model.rho_0, tf.transpose(model.rho_0, conjugate=True))
+
+    def testHInitialization(self):
+
+        test_H_diag = np.random.rand(hparams.bond_dim).astype(dtype=np.float32)
+        model = RhoCMPS(hparams, H_in=test_H_diag)
+
+        with self.cached_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            self.assertAllClose(model.H, np.diag(test_H_diag))
+
+    def testTrivialUpdate(self):
+        """
+        Update with H=R=0
+        """
+
+        test_H_diag = np.zeros([hparams.bond_dim], dtype=np.float32)
+        test_R = np.zeros(2*[hparams.bond_dim], dtype=np.float32)
+        signal = np.random.rand(hparams.minibatch_size).astype(dtype=np.float32)
+        model = RhoCMPS(hparams, H_in=test_H_diag, Rx_in=test_R, Ry_in=test_R)
+
+        with self.cached_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            stack_rho_0 = tf.stack(hparams.minibatch_size * [model.rho_0])
+            updated_rho = model._update_ancilla_rho(stack_rho_0, signal)
+            self.assertAllClose(stack_rho_0, updated_rho)
+
 
 
 if __name__ == '__main__':
