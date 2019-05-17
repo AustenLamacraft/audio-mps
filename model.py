c4ad2123c7a6d9d79bfd38c9ed7d0081a825807e
mencia <beinat.mencia@gmail.com>
2019-05-17 09:33:12 +0100

    I have changed the loss definition so that it is a batch average.

--- model.py
+++ model.py
@@ -133,7 +133,7 @@ class RhoCMPS(CMPS):
         data = tf.transpose(data, [1, 0])  # foldl goes along the 1st dimension
         _, loss = tf.foldl(self._rho_and_loss_update, data,
                            initializer=(rho_0, loss), name="loss_fold")
-        return tf.reduce_sum(loss)
+        return tf.reduce_mean(loss)
 
     def _rho_update(self, rho_and_loss, signal):
         # TODO change the name of the first argument
